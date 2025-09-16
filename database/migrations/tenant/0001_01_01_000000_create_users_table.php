462dfd2c44f5f8a2d8391089386fdcb6bd873bf5
bibin francis <bibin.francis@cubettech.com>
2025-09-16 18:39:43 +0530

    feat: started multitenancy

---
+++ database/migrations/tenant/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Enums\UserStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('password');
+
+            $table->string('email')->nullable()->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable()->unique();
+            $table->timestamp('phone_verified_at')->nullable();
+
+            $table->string('provider')->nullable()->comment('provider name, google, linkedin, etc...');
+            $table->string('provider_id')->nullable();
+            $table->rememberToken();
+
+            $table->string('status')->default(UserStatus::PENDING);
+            $table->timestamp('status_updated_at')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token')->comment('token or hashed OTP');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -19,131 +19,136 @@ use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Public Routes
-|--------------------------------------------------------------------------
-*/
 
-Route::get('/', fn () => response()->json(['message' => 'Ping successful!']));
-// Route::post('enquiry', [EnquiryController::class, 'store']);
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
 
-/*
-|--------------------------------------------------------------------------
-| Auth Routes
-|--------------------------------------------------------------------------
-*/
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+        /*
+        |--------------------------------------------------------------------------
+        | Public Routes
+        |--------------------------------------------------------------------------
+        */
 
-Route::prefix('auth')->group(function () {
-    Route::post('send-reset-otp', [PasswordResetController::class, 'sendPasswordResetOtp']);
-    Route::post('verify-otp', [PasswordResetController::class, 'verifyOtp']);
-    Route::post('reset-password', [PasswordResetController::class, 'resetPassword']);
-});
+        Route::get('/', fn() => response()->json(['message' => 'Ping successful!']));
+        // Route::post('enquiry', [EnquiryController::class, 'store']);
 
-/*
+        /*
+        |--------------------------------------------------------------------------
+        | Auth Routes
+        |--------------------------------------------------------------------------
+        */
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+
+        Route::prefix('auth')->group(function () {
+            Route::post('send-reset-otp', [PasswordResetController::class, 'sendPasswordResetOtp']);
+            Route::post('verify-otp', [PasswordResetController::class, 'verifyOtp']);
+            Route::post('reset-password', [PasswordResetController::class, 'resetPassword']);
+        });
+
+        /*
 |--------------------------------------------------------------------------
 | Authenticated Routes
 |--------------------------------------------------------------------------
 */
-Route::middleware(['auth:api'])->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('logout-all-devices', [AuthController::class, 'logoutFromAllDevices']);
+        Route::middleware(['auth:api'])->group(function () {
+            Route::post('logout', [AuthController::class, 'logout']);
+            Route::post('logout-all-devices', [AuthController::class, 'logoutFromAllDevices']);
 
-    Route::prefix('dashboard')->group(function () {
-        Route::get('/', [DashboardController::class, 'home']);
-        Route::get('layout', [DashboardController::class, 'getLayout']);
-        Route::post('layout', [DashboardController::class, 'updateLayout']);
-    });
+            Route::prefix('dashboard')->group(function () {
+                Route::get('/', [DashboardController::class, 'home']);
+                Route::get('layout', [DashboardController::class, 'getLayout']);
+                Route::post('layout', [DashboardController::class, 'updateLayout']);
+            });
 
-    Route::prefix('notifications')->group(function () {
-        Route::get('/', [NotificationController::class, 'index']);
-        Route::get('unread', [NotificationController::class, 'unread']);
-        Route::post('read/{id?}', [NotificationController::class, 'markAsRead']);
-    });
+            Route::prefix('notifications')->group(function () {
+                Route::get('/', [NotificationController::class, 'index']);
+                Route::get('unread', [NotificationController::class, 'unread']);
+                Route::post('read/{id?}', [NotificationController::class, 'markAsRead']);
+            });
 
-    Route::prefix('profile')->group(function () {
-        Route::get('/', [UserProfileController::class, 'show']);
-        Route::post('/', [UserProfileController::class, 'update']);
-        Route::post('/address', [UserProfileController::class, 'updateAddress']);
-        Route::post('/social-links', [UserProfileController::class, 'updateSocialLinks']);
-        Route::post('profile-image', [UserProfileController::class, 'updateProfileImage']);
-        Route::delete('profile-image', [UserProfileController::class, 'removeProfileImage']);
+            Route::prefix('profile')->group(function () {
+                Route::get('/', [UserProfileController::class, 'show']);
+                Route::post('/', [UserProfileController::class, 'update']);
+                Route::post('/address', [UserProfileController::class, 'updateAddress']);
+                Route::post('/social-links', [UserProfileController::class, 'updateSocialLinks']);
+                Route::post('profile-image', [UserProfileController::class, 'updateProfileImage']);
+                Route::delete('profile-image', [UserProfileController::class, 'removeProfileImage']);
+            });
 
-    });
-
-    /*
+            /*
     |--------------------------------------------------------------------------
     | Accounting
     |--------------------------------------------------------------------------
     */
-    // chart of account groups
-    // Route::apiResource('chart-of-account', ChartOfAccountController::class);
-    // taxes
-    // tax groups
-    // item tax types
-    // currency
-    // exchange rate
-    // gl settings
+            // chart of account groups
+            // Route::apiResource('chart-of-account', ChartOfAccountController::class);
+            // taxes
+            // tax groups
+            // item tax types
+            // currency
+            // exchange rate
+            // gl settings
 
-    /*
+            /*
     |--------------------------------------------------------------------------
     | Inventory
     |--------------------------------------------------------------------------
     */
-    Route::apiResource('category', CategoryController::class);
-    Route::apiResource('unit', UnitController::class);
-    Route::apiResource('item', ItemController::class);
-    Route::apiResource('batch', BatchController::class);
-    // warehouse
+            Route::apiResource('category', CategoryController::class);
+            Route::apiResource('unit', UnitController::class);
+            Route::apiResource('item', ItemController::class);
+            Route::apiResource('batch', BatchController::class);
+            // warehouse
 
-    /*
+            /*
     |--------------------------------------------------------------------------
     | Customer & Sales
     |--------------------------------------------------------------------------
     */
-    Route::apiResource('customer', CustomerController::class);
-    Route::get('sale/next-invoice-number', [SaleController::class, 'getNextInvoiceNumber']);
-    Route::apiResource('sale', SaleController::class);
+            Route::apiResource('customer', CustomerController::class);
+            Route::get('sale/next-invoice-number', [SaleController::class, 'getNextInvoiceNumber']);
+            Route::apiResource('sale', SaleController::class);
 
-    /*
+            /*
     |--------------------------------------------------------------------------
     | Vendor & Purchases
     |--------------------------------------------------------------------------
     */
-    Route::apiResource('vendor', VendorController::class);
-    Route::get('purchase/next-invoice-number', [PurchaseController::class, 'getNextInvoiceNumber']);
-    Route::apiResource('purchase', PurchaseController::class);
+            Route::apiResource('vendor', VendorController::class);
+            Route::get('purchase/next-invoice-number', [PurchaseController::class, 'getNextInvoiceNumber']);
+            Route::apiResource('purchase', PurchaseController::class);
 
-    /*
+            /*
     |--------------------------------------------------------------------------
     | Reports
     |--------------------------------------------------------------------------
     */
 
-    /*
+            /*
     |--------------------------------------------------------------------------
     | Others
     |--------------------------------------------------------------------------
     */
-    /* User Management */
-    Route::post('user/{user}/send-mail', [UserController::class, 'sendMail']);
-    Route::apiResource('user', UserController::class);
+            /* User Management */
+            Route::post('user/{user}/send-mail', [UserController::class, 'sendMail']);
+            Route::apiResource('user', UserController::class);
 
-    /* Role Management */
-    Route::apiResource('role', RoleController::class);
-    Route::get('permissions', [PermissionController::class, 'index']);
+            /* Role Management */
+            Route::apiResource('role', RoleController::class);
+            Route::get('permissions', [PermissionController::class, 'index']);
 
-    // update settings
+            // update settings
 
-});
+        });
 
-/*
+        /*
 |--------------------------------------------------------------------------
 | Webhook Callback Routes
 |--------------------------------------------------------------------------
 */
-Route::prefix('callback')->middleware('log.webhook')->group(function () {
-    Route::get('/', fn () => response()->json(['message' => 'Webhook ping successful!']));
-});
+        Route::prefix('callback')->middleware('log.webhook')->group(function () {
+            Route::get('/', fn() => response()->json(['message' => 'Webhook ping successful!']));
+        });
+    });
+}
